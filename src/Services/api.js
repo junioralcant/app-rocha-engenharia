1542e66118804e8ce2122d8c743fd6adc4867850
junior.marques <junior.marques@deixanoazul.com.br>
2022-08-25 15:37:05 -0300

    chore: gradle.bundle config for build in App center

--- src/Services/api.js
+++ src/Services/api.js
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 const api = axios.create({
   baseURL: 'https://api-to-ligado.herokuapp.com/',
-  // baseURL: 'http://192.168.0.105:3001/',
+  // baseURL: 'http://192.168.0.101:3001/',
 });
 
 api.interceptors.request.use(async (config) => {
