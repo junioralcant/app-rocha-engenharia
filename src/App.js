b1bc61c59cb56b1166a4b1ba689d14890dfacc2e
junioralcant <junioralcant111@gmail.com>
2022-08-31 15:24:08 -0300

    chore: config code push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import codePush from 'react-native-code-push';
 
 import Routes from './routes';
 
-export default function App() {
+function App() {
+  useEffect(() => {
+    codePush.sync({
+      installMode: codePush.InstallMode.IMMEDIATE,
+    });
+  }, []);
+
   return <Routes />;
 }
+
+export default codePush({
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+})(App);
